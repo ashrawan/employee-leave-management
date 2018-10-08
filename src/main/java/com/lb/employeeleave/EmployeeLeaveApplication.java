06486044c45527a5bb1dd66cb9582d100e4e3987
ashrawan <shrawan.adhikari.31@gmail.com>
2018-10-08 04:57:49 -0700

    refactored

--- src/main/java/com/lb/employeeleave/EmployeeLeaveApplication.java
+++ src/main/java/com/lb/employeeleave/EmployeeLeaveApplication.java
@@ -1,19 +1,14 @@
 package com.lb.employeeleave;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class EmployeeLeaveApplication {
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
     public static void main(String[] args) {
+//        PasswordEncoder encoder = new BCryptPasswordEncoder();
+//        System.out.println( encoder.encode("admin"));
         SpringApplication.run(EmployeeLeaveApplication.class, args);
     }
 }
